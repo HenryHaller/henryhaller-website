e66be453ce89a15f448c1ed9cca67bdac02b1182
Henry Haller <henrybhaller@gmail.com>
2018-06-24 16:19:20 +0000

    Age feature is much improved.

--- static/js/EA.js
+++ static/js/EA.js
@@ -1,18 +1,20 @@
-//object.onload = function(){
 let tds = document.getElementsByClassName("timestamp");
 let now = Date.now() / 1000;
 for (index = 0; index < tds.length; index++) {
   let td = tds[index];
   let insertion_date = new Date(td.innerHTML);
-  //console.log(insertion_date);
   seconds = insertion_date.getTime() / 1000;
-  let elapsed_time = now - seconds;
-  let elapsed_text = "Added less than 1 Minute Ago."
-  if (elapsed_time > 60) {elapsed_text = "Added less than 1 Hour Ago.";}
-  if (elapsed_time > (60 * 60)) {elapsed_text = "Added less than 6 Hours Ago.";}
-  if (elapsed_time > (60 * 60 * 12)) {elapsed_text = "Added less than 12 Hours Ago.";}
-  if (elapsed_time > (60 * 60 * 24)) {elapsed_text = "Added more than a day ago 24 hours.";}
+  let elapsed_time = now - seconds; //Time in seconds since insertion date.
+  const minute = 60
+  const hour = 3600
+  const day = 86400
+  const week = 604800
+  let minutes = (elapsed_time % hour) / minute
+  let hours = (elapsed_time % day) / hour
+  let days = (elapsed_time % week) / day
+  let elapsed_text = Math.floor(minutes) +'  Minutes.'
+  if (hours > 1.000)  { elapsed_text = Math.floor(hours) + " Hours, " + elapsed_text}
+  if (days > 1.000)  { elapsed_text = Math.floor(days) + " Days " + elapsed_text}
+  console.log(days, hours, minutes);
   td.innerHTML = elapsed_text;
-
   }
-//}
