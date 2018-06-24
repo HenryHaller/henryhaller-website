5558e753e9bd26bf36050217d91aeda59250afe3
Henry Haller <henrybhaller@gmail.com>
2018-06-24 19:22:51 +0000

    schema.py defines a class for episode data.

---
+++ schema.py
@@ -0,0 +1,16 @@
+import hashlib
+class Episode():
+	show_title = None
+	episode_title = None
+	url = None
+	color = None
+	timestamp = None
+	def __init__(self, rows):
+		self.show_title = rows[0]
+		self.url = rows[1]
+		self.episode_title = rows[2]
+		self.timestamp = rows[3]
+		h = hashlib.new('sha256')
+		h.update(self.show_title.encode('utf-8'))
+		self.color = h.hexdigest()[0:6]
+
