ca56899a9f3477defab57e9d24bca7a67d1331b8
Henry Haller <henrybhaller@gmail.com>
2018-08-19 23:39:10 +0900

    correctly try to load sesion key

--- my_session_key.py
+++ my_session_key.py
@@ -2,7 +2,7 @@ import os
 
 
 if 'SESSION_KEY' in os.environ:
-	my_session_key = os.environ['DATABASE_URL']
+	my_session_key = os.environ['SESSION_KEY']
 else:
 	import session_key
 	my_session_key = session_key.key
